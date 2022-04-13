e3cda632321c13f8ccf763cffd8017e38928822c
SrtaRavell <raphaellyravell@gmail.com>
2022-04-13 10:49:19 -0300

    início da home e roleta

--- src/modo_active.js
+++ src/modo_active.js
@@ -1,35 +1,36 @@
-function click_modo(){
+function click_modo() {
     const manual = document.querySelector('.manual')
     const auto = document.querySelector('.auto')
     
+
     manual.addEventListener('click', () => {
         manual.classList.add('modo_active')
         auto.classList.remove('modo_active')
-        document.querySelectorAll('[modo]').forEach(link =>{
+        document.querySelector('[modo]').forEach((link) => {
             const conteudo = document.querySelector('.conteudo_modo')
 
-            link.onclick = function(e) {
-                e.preventDefault()
+            link.onclick = function (e) {
+                e.preventDefault();
                 fetch(link.getAttribute('modo'))
-                    .then(resp => resp.text())
-                    .then(html => conteudo.innerHTML = html)
-            }
-        })
-    })
+                    .then((resp) => resp.text())
+                    .then((html) => (conteudo.innerHTML = html));
+            };
+        });
+    });
 
     auto.addEventListener('click', () => {
         auto.classList.add('modo_active')
         manual.classList.remove('modo_active')
-        document.querySelectorAll('[modo]').forEach(link =>{
+        document.querySelector('[modo]').forEach(link => {
             const conteudo = document.querySelector('.conteudo_modo')
 
-            link.onclick = function(e) {
+            link.onclick = function (e) {
                 e.preventDefault()
                 fetch(link.getAttribute('modo'))
                     .then(resp => resp.text())
                     .then(html => conteudo.innerHTML = html)
-            }
-        })
-    })
+            };
+        });
+    });
 }
 click_modo()
